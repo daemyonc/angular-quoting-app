23f3bcefa01786c05d7b746742961091ec658b07
Daemyon <damioncms@gmail.com>
2020-05-07 18:23:22 -0500

    Add variables and function to calculate results

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { decoPricing } from './data/decoPricing';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-quoting-app';
-}
+
+  itemCost: number;
+  blankCost: number;
+  decoMeth: string;
+  qty: number;
+  decoPrice: number;
+
+  constructor() {
+    this.decoMeth = 'SP';
+    this.qty = 12;
+  }
+
+  calculateResults(cost: number, deco: string = 'SP', qty: number = 12) {
+    this.itemCost = cost;
+    this.blankCost = parseFloat((cost / .79).toFixed(2));
+    this.decoPrice = parseFloat((this.blankCost + decoPricing[deco][qty]).toFixed(2));
+    console.log(cost,deco,qty);
+  }
+ }
